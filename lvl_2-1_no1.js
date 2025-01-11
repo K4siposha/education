ec5aa8c8b7b9226e33ab031a66b5ffa606625bb4
Дмитрий Беспалов <dmitrij.bespalov.98@mail.ru>
2025-01-11 08:34:09 +0300

    Уроки

---
+++ lvl_2-1_no1.js
@@ -0,0 +1,6 @@
+//Дана некоторая строка. Найдите позицию первого нуля в строке.
+// let str = ('may 01');
+// console.log(str.indexOf('0'));
+
+let str = ('may 01');
+console.log(1 + str.indexOf('0'));
